6ce269daed89385841e27c571e455e8840a30dd7
Ankit Kathuria <ankitkathuria@Ankits-Air.fritz.box>
2018-12-11 01:29:28 +0530

    Date Support in Script

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-BHAVCOPY_URL = "https://www.bseindia.com/download/BhavCopy/Equity/EQ071218_CSV.ZIP"
+BHAVCOPY_URL = "https://www.bseindia.com/download/BhavCopy/Equity/"
 FIELDS = ["SC_NAME", "SC_CODE", "OPEN", "HIGH", "LOW", "CLOSE"]
 PRIMARY_KEY = ["SC_NAME" , "SC_CODE"]
 REDIS_URL= os.environ.get('REDIS_URL', "redis://localhost:6379")
+DATE_FORMAT = "%d-%m-%y"
